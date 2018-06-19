4a6d22421f287494c3389e6adad749ebdb549600
Fernando Pavanello <fernando.pavanello2@gmail.com>
2018-06-19 02:00:45 +0000

    Primeiro progama e String em Python

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+message = "Hello Python World"
+print(message)
+
+message = "Passei por cima"
+print(message)
+
+name = "fErNaNdo pAvAnEllO"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+print("\tPython")  # Tab
+print("Exemplos:\nUm\nDois\nTres")  # Quebrar Linha
+print("a", "b", "c")
